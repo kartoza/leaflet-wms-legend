120173a93efb4d0e4acd4b9ffbd5b51cbf866e37
James Goldie (Rensa) <me@rensa.co>
2018-03-14 14:25:58 +1100

    Defer sizing to CSS and use a class to signify collapsed state

--- leaflet.wmslegend.js
+++ leaflet.wmslegend.js
@@ -34,9 +34,8 @@ L.Control.WMSLegend = L.Control.extend({
         // toggle legend visibility
         var style = window.getComputedStyle(this.img);
         if (style.display === 'none') {
-            this.container.style.height = this.height + 'px';
-            this.container.style.width = this.width + 'px';
             this.img.style.display = this.displayStyle;
+            L.DomUtil.removeClass(this.container, 'wms-legend-collapsed');
         }
         else {
             if (this.width === null && this.height === null) {
@@ -47,8 +46,7 @@ L.Control.WMSLegend = L.Control.extend({
             }
             this.displayStyle = this.img.style.display;
             this.img.style.display = 'none';
-            this.container.style.height = '20px';
-            this.container.style.width = '20px';
+            L.DomUtil.addClass(this.container, 'wms-legend-collapsed');
         }
     },
 
